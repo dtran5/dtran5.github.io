7bbdde9995370118a86d64b141506b015b28dbfb
Dan <dan.tran301@gmail.com>
2020-10-08 16:40:05 -0700

    Fixed responsive design and changed image cards and images

--- js/main.js
+++ js/main.js
@@ -2,8 +2,25 @@
 window.addEventListener('scroll', () => {
     const nav = document.querySelector('nav');
     const windowPosition = window.scrollY > 0;
+    const windowWidth = window.innerWidth
 
-    nav.classList.toggle('scroll-active', windowPosition)
+    // if (windowWidth < 991.98) {
+    //     nav.classList.toggle('scroll-active-medium', windowPosition)
+    // } else {
+        // nav.classList.toggle('scroll-active', windowPosition)
+    
+    if (windowWidth < 991.98) {
+        nav.classList.remove('scroll-active')
+        nav.classList.remove('navbar-custom')
+        nav.classList.add('navbar-custom-medium-active')
+        nav.classList.toggle('scroll-active-medium', windowPosition)
+        nav.classList.toggle('navbar-custom-medium', windowPosition)
+    } else {
+        nav.classList.remove('scroll-active-medium')
+        nav.classList.add('navbar-custom')
+        nav.classList.toggle('scroll-active', windowPosition)
+    }
+    
     
 })
 
